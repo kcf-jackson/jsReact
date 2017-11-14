4a117fe8a1e0e2f2bc02e6a5fa4e12c02fe4eee6
kcf-jackson <kcf.jackson@gmail.com>
2017-11-15 00:21:23 +1100

    Simplify example 2

--- inst/example_2.R
+++ inst/example_2.R
@@ -5,23 +5,20 @@ library(magrittr)
 my_html <- create_html() %>%
   add_js_library("plotly") %>%
   add_title("Send message") %>%
-  add_slider(type = "range", id = "slide_input", min = "0", max = "100",
-             oninput = "show_value(this.Value)") %>%
+  add_slider(min = "0", max = "100", oninput = "show_value(value)") %>%
   add_title("Receive message") %>%
   add_div(id = "output") %>%
   add_div(id = "plotly_plot")
 my_html %<>% add_script("
-  function show_value() {
-    var input = document.getElementById('slide_input');
-    ws.send(input.value);
+  function show_value(value) {
+    ws.send(value);
   }
   ws.onmessage = function(msg) {
     document.getElementById('output').innerHTML = msg.data;
     var data0 = JSON.parse(msg.data);
-    var px = data0['x'][0];
-    var py = data0['y'][0];
-    var trace1 = {x: [px], y: [py], mode: 'markers', type: 'scatter'};
-    var layout = {xaxis:{range: [-10, 10]}, yaxis: {range: [-20, 120]}};
+    var trace1 = {x: data0['x'], y: data0['y'], mode: 'markers', type: 'scatter'};
+    var layout = {xaxis:{range: [-4, 4]}, yaxis: {range: [-20, 120]},
+                  margin: {l:35, r:20, b:35, t:20}, width:550, height:400};
     Plotly.newPlot('plotly_plot', [trace1], layout);
   }
 ")
